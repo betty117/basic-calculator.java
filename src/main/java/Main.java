62b0369f7149f7463bf2c49b7c4b6d39716a7668
betty117 <bettyh63@myyahoo.com>
2025-09-25 13:02:03 -0500

    Change may some staff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+       Scanner input = new Scanner(System.in);
+       System.out.println("Please enter the first number:");
+       double firstNumber = input.nextDouble();
+
+        System.out.print("Enter the second number: ");
+       double SecondNumber = input.nextDouble();
+
+       System.out.println("operation would you like to preform");
+       System.out.println("1. Addition");
+       System.out.println("2. Subtraction");
+       System.out.println("3. Multiplication");
+       System.out.println("4. Division");
+       System.out.println("enter your choice (1-4);");
+       int choice = input.nextInt();
+       double result = firstNumber - SecondNumber;
+       System.out.println("The result of Substraction is : " + result);
+       input.close();
+
+
+
+    }
 }
